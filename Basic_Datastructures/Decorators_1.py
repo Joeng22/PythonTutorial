0ff1adb434b65a80ec1fdaa101f428b8534ecdb2
Joe <joeng22@gmail.com>
2024-07-09 09:23:08 +0530

    Decorators in python

---
+++ Basic_Datastructures/Decorators_1.py
@@ -0,0 +1,25 @@
+#Python decorator tutorial
+#How to convert lower case to upper case using decorators
+'''
+Python Decorators Tutorial
+    A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without modifying its structure. 
+    Decorators are typically applied to functions, and they play a crucial role in enhancing or modifying the behavior of functions.
+'''
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+
+    return wrapper
+
+
+@uppercase_decorator
+def say_hi():
+    return 'hello world'
+
+decorate = uppercase_decorator(say_hi)
+print("Normal function call ",decorate())
+
+
+print("Using decoratore @:",say_hi())
--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Possible interview questions for python Deep learning, Machine learning
 1 . Basic difference between List and tuple
     List is mutable
     Tuple is immutable
+
+2. Python Decorators Tutorial
+    A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without modifying its structure. 
+    Decorators are typically applied to functions, and they play a crucial role in enhancing or modifying the behavior of functions.
